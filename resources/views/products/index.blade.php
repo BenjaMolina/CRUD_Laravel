a9296461e537277297e7e063f9886110dd848f51
BenjaMolina <molinasb.94@gmail.com>
2018-08-11 12:16:36 -0500

    Creacion de plantilla blade index, layout - Herencia

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    <div class="col-sm-8">
+        <h3>Listado de productos</h3>
+    </div>
+    <div class="col-sm-4">
+        <h3>Mensaje</h3>
+    </div>
+@endsection
